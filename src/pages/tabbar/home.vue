8141efd105392d90c5a8fb74d00f5c001e39c4c3
郝宇婕 <haoyujie@jingnuodata.com>
2026-06-09 16:55:50 +0800

    fix: 隐藏首页群聊入口

--- src/pages/tabbar/home.vue
+++ src/pages/tabbar/home.vue
@@ -103,7 +103,7 @@
             </scroll-view>
           </view>
           <!-- 群聊start -->
-          <view class="commonTitleBox">
+          <!-- <view class="commonTitleBox">
             <view class="title navTitle">
               <image src="/static/images/nft@2x.png" class="tb" alt="" />
               {{ t('home.title.groupChat') }}
@@ -118,7 +118,7 @@
               height="100%"
               custom-class="chatGroupImg"
             ></wd-img>
-          </view>
+          </view> -->
           <view class="commonTitleBox">
             <view class="title navTitle">
               <image src="/static/images/nft@2x.png" class="tb" alt="" />
