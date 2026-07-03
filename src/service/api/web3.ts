421c253b202a370c16285e0d8a7195b05f61478e
DaysJan <daysjan1993@gmail.com>
2026-07-04 02:25:40 +0800

    feat: add payment app selection and nft valuation

--- src/service/api/web3.ts
+++ src/service/api/web3.ts
@@ -2,12 +2,14 @@ import { http } from '@/utils/http'
 
 type CreateWebDataForKeyApiResponse = {
   key: string
+  url: string
 }
 
-export const createWebDataForKeyApi = (op: string, extend?: any) => {
+export const createWebDataForKeyApi = (op: string, extend?: any, lang?: string) => {
   return http.post<CreateWebDataForKeyApiResponse>('/v1/web3/web3/create-web3-data-for-key', {
     op,
     extend,
+    lang,
   })
 }
 
