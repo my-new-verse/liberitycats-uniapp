fa7e460740c1f3e3d34ddd0b7f6fe73f1d546cd2
郝宇婕 <haoyujie@jingnuodata.com>
2026-07-14 21:28:39 +0800

    fix: 修改图片压缩规则

--- src/components/discover/SocialTab.vue
+++ src/components/discover/SocialTab.vue
@@ -1077,15 +1077,7 @@ const goSearch = () => {
 }
 
 const doHandlePreview = (images: string[], currentIndex: number = 0) => {
-  // images = images.map((item) => (item = item + '?x-oss-process=style/sqdt'))
-  const ext = 'webp' // 或其他
-  // images = `${path.split('?')[0]}?x-oss-process=image/auto-orient,1/resize,w_600,m_lfit/quality,q_100/format,${ext}`;
-  images = images.map(
-    (item) =>
-      (item =
-        item +
-        `?x-oss-process=image/auto-orient,1/resize,w_600,m_lfit/quality,q_100/format,${ext}`),
-  )
+  images = images.map((item) => (item = item + '?x-oss-process=style/sqdt'))
   handlePreview(images, currentIndex)
 }
 </script>
